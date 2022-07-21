89124949982062fd8ee12b651ea71ce9e8fa756e
Frank Hellwig <frank@hellwig.org>
2022-07-21 01:08:14 -0400

    vcard parsing

--- fastmail.js
+++ fastmail.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { DAVClient } from 'tsdav';
 import parser from 'vdata-parser';
 
@@ -19,6 +20,7 @@ export async function getFastmailContacts() {
   await client.login();
 
   const addressBooks = await client.fetchAddressBooks();
+  console.log(addressBooks);
   const vcards = await client.fetchVCards({
     addressBook: addressBooks[0]
   });
@@ -77,4 +79,70 @@ function parsePhoneNumberType(params) {
   } else {
     return 'unknown';
   }
+}
+
+/**
+ * Splits text into lines, each line separated by an optional carriage return
+ * followed by a line feed. Blank lines are not included in the returned array.
+ * Lines beginning with a space are unfolded and added to the previous line.
+ * @param  text the text to split
+ * @return an array of lines
+ */
+function splitTextIntoLines(text) {
+  const sep = new RegExp('\r?\n');
+  const split = text.split(sep);
+  const lines = [];
+  split.forEach((line) => {
+    if (line.length > 0) {
+      if (line.startsWith(' ') || line.startsWith('\t')) {
+        lines[lines.length - 1] += line.substring(1);
+      } else {
+        lines.push(line);
+      }
+    }
+  });
+  return lines;
+}
+
+/**
+ * Parses a line and returns an object having a name, optional type, and value.
+ * @param line the line to parse
+ * @return a {name, type, value} object
+ */
+function parseLine(line) {
+  const parts = line.split(':', 2);
+  const key = parts[0].split(';', 2);
+  if (key[1] && key[1].startsWith('TYPE=')) {
+    key[1] = key[1].substring(5);
+  } else {
+    key[1] = null;
+  }
+  return {
+    name: key[0],
+    type: key[1] || null,
+    value: parts[1]
+  };
+}
+
+/**
+ * Parses a vCard into an object where the keys are the vCard properties and
+ * the property is a {type, value} object. All returned object keys are lower
+ * case. The X-ADDRESSBOOKSERVER-KIND property is converted to 'kind' and any
+ * other names beginning with 'X-' are skipped.
+ * @param text the vCard text
+ * @return a parsed object
+ */
+function parseCard(text) {
+  const lines = splitTextIntoLines(text);
+  const vcard = {};
+  lines.forEach((line) => {
+    const obj = parseLine(line);
+    if (obj.name === 'X-ADDRESSBOOKSERVER-KIND') {
+      obj.name = 'KIND';
+    }
+    //if (!obj.name.startsWith('X-')) {
+    vcard[obj.name.toLowerCase()] = { type: obj.type, value: obj.value };
+    //}
+  });
+  return vcard;
 }
